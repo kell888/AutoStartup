a36c1eda76d597abfd0bc13d618a9fcec53f84b8
kell pang <kell pang>
2016-12-22 13:12:12 +0800

    2016-12-22

---
+++ 自启动配置工具/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace 自启动配置工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(572, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "浏览...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(163, 120);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(87, 22);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "注册表";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(327, 120);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "所有用户";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(474, 120);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(105, 22);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "当前用户";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "可执行程序：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(572, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 34);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "设置自启";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "启动宿主：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(467, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 34);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "取消自启";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "EXE文件(*.exe)|*.exe|BAT文件(*.bat)|*.bat|COM文件(*.com)|*.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 307);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "自启动程序配置工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 自启动配置工具/Form1.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 自启动配置工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 读取注册表
+        /// </summary>
+        /// <returns></returns>
+        public static string ReadRegistry(string name)
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey softWare = rk.OpenSubKey("Software");
+                RegistryKey microsoft = softWare.OpenSubKey("Microsoft");
+                RegistryKey windows = microsoft.OpenSubKey("Windows");
+                RegistryKey current = windows.OpenSubKey("CurrentVersion");
+                RegistryKey run = current.OpenSubKey(@"Run", true);
+                return run.GetValue(name, "", RegistryValueOptions.None).ToString();
+            }
+            catch// (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 设置开机自启动-写入注册表
+        /// </summary>
+        /// <param name="exePath">可执行文件的完整路径</param>
+        /// <returns></returns>
+        public static bool SetSelfStarting(string exePath)
+        {
+            try
+            {
+                string name = Path.GetFileName(exePath);
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey softWare = rk.OpenSubKey("SOFTWARE");
+                RegistryKey microsoft = softWare.OpenSubKey("Microsoft");
+                RegistryKey windows = microsoft.OpenSubKey("Windows");
+                RegistryKey current = windows.OpenSubKey("CurrentVersion");
+                RegistryKey run = current.OpenSubKey(@"Run", true);//这里必须加true就是得到写入权限 
+                run.SetValue(name, exePath);
+                run.Close();
+                return true;
+            }
+            catch// (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 取消开机自启-删除注册表
+        /// </summary>
+        /// <param name="name">注册表项的名字</param>
+        /// <returns></returns>
+        public static bool CancelSelfStarting(string name)
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey softWare = rk.OpenSubKey("Software");
+                RegistryKey microsoft = softWare.OpenSubKey("Microsoft");
+                RegistryKey windows = microsoft.OpenSubKey("Windows");
+                RegistryKey current = windows.OpenSubKey("CurrentVersion");
+                RegistryKey run = current.OpenSubKey(@"Run", true);
+                run.DeleteValue(name);//删除注册表的值
+                run.Close();
+                return true;
+            }
+            catch// (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void AutoStartup(string executablePath, bool flag)
+        {
+            string filename = Path.GetFileName(executablePath);
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.System).Substring(0, 1) + @":\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup";
+            if (!Directory.Exists(path))
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string file = path + "\\" + filename + ".lnk";
+            if (flag)
+            {
+                if (!IsAutoStartupAllUsers(filename))
+                {
+                    try
+                    {
+                        CreateShortcut(executablePath, path);
+                    }
+                    catch (Exception e)
+                    {
+                        if (!IsAutoStartupCurrentUser(filename))
+                            CreateShortcut(executablePath, Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+                    }
+                }
+                else if (!IsAutoStartupCurrentUser(filename))
+                {
+                    CreateShortcut(executablePath, Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+                }
+            }
+            else
+            {
+                if (IsAutoStartupAllUsers(filename))
+                {
+                    File.Delete(file);
+                }
+                if (IsAutoStartupCurrentUser(filename))
+                {
+                    string file2 = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" + filename + ".lnk";
+                    File.Delete(file2);
+                }
+            }
+        }
+
+        public void SetAutoStartupAllUsers(string executablePath)
+        {
+            string filename = Path.GetFileName(executablePath);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.System).Substring(0, 1) + @":\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup";
+            if (!Directory.Exists(path))
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string file = path + "\\" + filename + ".lnk";
+            if (!IsAutoStartupAllUsers(filename))
+            {
+                try
+                {
+                    CreateShortcut(executablePath, path);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+
+        public void SetAutoStartupCurrentUser(string executablePath)
+        {
+            string filename = Path.GetFileName(executablePath);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string file = path + "\\" + filename + ".lnk";
+            if (!IsAutoStartupAllUsers(filename))
+            {
+                try
+                {
+                    CreateShortcut(executablePath, path);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+
+        public void CancelAutoStartupAllUsers(string executablePath)
+        {
+            string filename = Path.GetFileName(executablePath);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.System).Substring(0, 1) + @":\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup";
+            string file = path + "\\" + filename + ".lnk";
+            if (IsAutoStartupAllUsers(filename))
+            {
+                File.Delete(file);
+            }
+        }
+
+        public void CancelAutoStartupCurrentUser(string executablePath)
+        {
+            string filename = Path.GetFileName(executablePath);
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string file = path + "\\" + filename + ".lnk";
+            if (IsAutoStartupCurrentUser(filename))
+            {
+                File.Delete(file);
+            }
+        }
+
+        private static void CreateShortcut(string executablePath, string path)
+        {
+            // 声明操作对象
+            IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShellClass();
+            string filename = Path.GetFileName(executablePath);
+            string file = path + "\\" + filename + ".lnk";
+            // 创建一个快捷方式
+            IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(file);
+            // 关联的程序
+            shortcut.TargetPath = executablePath;
+            // 参数
+            //shortcut.Arguments = "";
+            // 快捷方式描述，鼠标放到快捷方式上会显示出来哦
+            shortcut.Description = filename + "应用程序";
+            // 全局热键
+            //shortcut.Hotkey = "CTRL+SHIFT+N";
+            // 设置快捷方式的图标，这里是取程序图标，如果希望指定一个ico文件，那么请写路径。
+            //shortcut.IconLocation = "notepad.exe, 0";
+            // 保存，创建就成功了。
+            shortcut.Save();
+        }
+
+        private bool IsAutoStartupAllUsers(string filename)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.System).Substring(0, 1) + @":\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup";
+            if (Directory.Exists(path))
+            {
+                string lnk = path + "\\" + filename + ".lnk";
+                string[] files = Directory.GetFiles(path, "*.lnk", SearchOption.TopDirectoryOnly);
+                foreach (string file in files)
+                {
+                    if (file.Equals(lnk, StringComparison.InvariantCultureIgnoreCase))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsAutoStartupCurrentUser(string filename)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string lnk = path + "\\" + filename + ".lnk";
+            string[] files = Directory.GetFiles(path, "*.lnk", SearchOption.TopDirectoryOnly);
+            foreach (string file in files)
+            {
+                if (file.Equals(lnk, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            openFileDialog1.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = textBox1.Text.Trim();
+            if (File.Exists(file))
+            {
+                if (radioButton1.Checked)
+                    SetSelfStarting(file);
+                else if (radioButton2.Checked)
+                    SetAutoStartupAllUsers(file);
+                else if (radioButton3.Checked)
+                    SetAutoStartupCurrentUser(file);
+                MessageBox.Show("设置成功");
+            }
+            else
+            {
+                MessageBox.Show("指定的文件不存在！请确认。");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string file = textBox1.Text.Trim();
+            if (radioButton1.Checked)
+                CancelSelfStarting(file);
+            else if (radioButton2.Checked)
+                CancelAutoStartupAllUsers(file);
+            else if (radioButton3.Checked)
+                CancelAutoStartupCurrentUser(file);
+            MessageBox.Show("取消成功");
+        }
+    }
+}
